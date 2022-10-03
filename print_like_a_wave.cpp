9b0f9669041c8c04cd479f810404c2ec9513422b
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-10-03 19:10:45 +0530

    Update print_like_a_wave.cpp

--- print_like_a_wave.cpp
+++ print_like_a_wave.cpp
@@ -1,20 +1,25 @@
-vector<int> print_like_a_wave(vector<vector<int>> arr,int n,int m){
-
+vector<int> wavePrint(vector<vector<int> > arr, int nRows, int mCols)
+{
     vector<int> ans;
-    for(int col=0;col<n;col++){
-
-        if(col&1){//odd case bottom to top
-            for (int row=m-1;row>=0;row--){
-                cout<<arr[row][col]<<" ";
+    
+    for(int col=0; col<mCols; col++) {
+        
+        if( col&1 ) {
+            //odd Index -> Bottom to top
+            
+            for(int row = nRows-1; row>=0; row--) {
+                //cout << arr[row][col] <<" ";
+                ans.push_back(arr[row][col]);
+            }    
+        }
+        else
+        {
+            // 0 or even iondex -> top to bottom
+            for(int row = 0; row<nRows; row++) {
+                //cout << arr[row][col] << " ";
                 ans.push_back(arr[row][col]);
             }
-        }
-        else{//even index case top to bottm
-            for (int row=0;row<=m;row++){
-                cout<<arr[row][col]<<" ";
-                ans.push_back(arr[row][col]);
-            }
-        }
+        }   
     }
     return ans;
 }
