7477c1d685f03678f02e09c34ede359244a84aaf
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-10-25 23:01:03 +0530

    Create maximum_subarray_sum_581.cpp

---
+++ maximum_subarray_sum_581.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    /
+    long long  sum=0;
+    long long  maxsum=INT_MIN;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+        if(sum<0){
+            sum=0;
+        }
+
+        maxsum=max(sum,maxsum);
+
+        
+         
+    }
+   return maxsum;
+}
