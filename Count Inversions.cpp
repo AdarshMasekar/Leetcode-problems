46ad279786a83b9b53696c7f322375363fe99ada
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-11-16 23:51:38 +0530

    Create Count Inversions.cpp

---
+++ Count Inversions.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+long long getInversions(long long *arr, int n){
+    // Write your code here.
+    long long count=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i]>arr[j] & i < j){
+                count++;
+            }
+        }
+    }
+    return count;
+}
