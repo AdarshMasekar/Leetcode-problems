70d11bb4730436e2492527a0c89fc1cde32af813
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-11-18 10:57:03 +0530

    Update 876. Middle of the Linked List.cpp

--- 876. Middle of the Linked List.cpp
+++ 876. Middle of the Linked List.cpp
@@ -8,13 +8,25 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
+    
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        // best solution with O(n/2) complexity
         ListNode *slow = head, *fast = head;
         while (fast && fast->next)
             slow = slow->next, fast = fast->next->next;
         return slow;
-    }
+        
+        // naive approach
+//         int count=0;
+//         ListNode *temp=head;
+//         while(temp){
+//             count++;
+//             temp = temp->next;
+//     }
+//         temp=head;
+//         for(int i=;i<n/2;i++)
+//             temp = temp -> next;
+//         return temp;
 };
