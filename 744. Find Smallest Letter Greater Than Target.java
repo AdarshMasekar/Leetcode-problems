f73efda7d21b622380d8296d102803482a4ebd2d
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-05-31 12:12:47 +0530

    Create 744. Find Smallest Letter Greater Than Target.java
    #binary_search 
    #arrays

---
+++ 744. Find Smallest Letter Greater Than Target.java
@@ -0,0 +1,17 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0, end = letters.length - 1;
+				char ans= letters[0];
+				while (start <= end) {
+					int mid = start + (end - start) / 2;
+					if (letters[mid] > target) {
+						ans = letters[mid];
+						end = mid - 1;
+					} else {
+						start = mid + 1;
+					}
+				}
+				System.gc();
+				return ans;
+    }
+}
