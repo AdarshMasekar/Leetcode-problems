f3d75c8dc5f789b22e6755cf46a9a7c4f99563e1
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-06-25 21:34:13 +0530

    Array String Simulation

---
+++ 2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for(String o : operations) x += (44 - o.charAt(1));
+        return x;
+    }
+}
