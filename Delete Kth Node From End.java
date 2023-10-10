d84e4a7b0bbe2aaf2282d51d4ea336233ec2c9a4
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-10-10 16:23:22 +0530

    Create Delete Kth Node From End.java

---
+++ Delete Kth Node From End.java
@@ -0,0 +1,55 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node removeKthNode(Node head, int K)
+    {
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node first = dummy;
+        Node second = dummy;
+
+        for(int i=0;i<=K;i++){
+            first = first.next;
+        }
+
+        while(first != null){
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+
+        return dummy.next;
+    }
+}
